adf14ed48ba084dd4ad0dc166261abd1c981780d
oliverdesa <107208238+oliverdesa@users.noreply.github.com>
2023-11-05 17:07:43 -0500

    Added code to build differential abundance figure

--- PRJEB10878_figure_building.R
+++ PRJEB10878_figure_building.R
@@ -2,6 +2,16 @@ library(feather)
 library(ggplot2)
 library(tidyverse)
 
+czm_clr <- function(count_df) {
+  
+  set.seed(101)
+  
+  clr <- t(apply(count_df, 1, function(x){log(x) - mean(log(x))}))
+  
+  clr
+  
+}
+
 pooled_data <- read_feather('pooled_abundance_data.feather')
 pooled_data <- as.data.frame(pooled_data)  
 
@@ -12,44 +22,63 @@ metadata <- as.data.frame(metadata)
 # properly on the SampleIdentifier column present in both dfs
 pooled_data$config <- metadata[match(pooled_data$SampleIdentifier, metadata$SampleIdentifier),]$config
 
-long_df <- pooled_data %>%
+identifiers_and_config <- pooled_data[, c("SampleIdentifier", "config")]
+
+# List of columns needed
+enzymes = c('Amidase', 'DD-carboxypeptidase', 'DD-endopeptidase', 'DL-endopeptidase', 
+            'Glucosaminidase', 'LD-carboxypeptidase', 'LD-endopeptidase', 'Muramidase')
+
+#make new DF with only these columns
+filtered_pooled_data <- pooled_data[enzymes]
+
+CLR_pooled_data <- czm_clr(filtered_pooled_data)
+
+CLR_pooled_data <- as.data.frame(CLR_pooled_data)
+
+# Add config column back into data frame
+CLR_pooled_data <- cbind(identifiers_and_config, CLR_pooled_data)
+
+
+long_df <- CLR_pooled_data %>%
   pivot_longer(
-    cols = starts_with("Amidase"):starts_with("UC118"),
+    cols = -c(SampleIdentifier, config),
     names_to = "Enzyme",
-    values_to = "Value"
+    values_to = "CLR Relative Abundance"
   )
 
-# Define new labels with the known sample sizes
-new_labels <- c(case = "Case (n=74)", control = "Control (n=53)")
+# Convert the 'CLR Relative Abundance' to numeric, coercing non-numeric to NAs
+long_df$`CLR Relative Abundance` <- as.numeric(as.character(long_df$`CLR Relative Abundance`))
 
-# Function to create a plot
-plot_function <- function(data, enzyme_name) {
-  p <- ggplot(data, aes(x = config, y = Value)) +
-    geom_boxplot(aes(fill = config)) +
-    scale_fill_manual(values = c("control" = "grey", "case" = "blue")) +
-    geom_jitter(width = 0.2, size = 1, color = 'black') +
-    labs(
-      title = paste(enzyme_name), 
-      x = "", 
-      y = "Reads per Kilobase (RPK)"
-    ) +
-    scale_x_discrete(labels = new_labels) +
-    theme_minimal(base_size = 12) +
-    theme(
-      plot.background = element_rect(fill = "white", color = NA),
-      panel.background = element_rect(fill = "white", color = NA),
-      axis.text.x = element_text(angle = 45, hjust = 1),
-      legend.position = "none"
-    )
-  
-  ggsave(paste0("Enzyme_", enzyme_name, ".png"), dpi = 600, plot = p, width = 10, height = 6)
-}
+# Check again after conversion
+str(long_df$`CLR Relative Abundance`)
 
-# Loop through each enzyme and plot
-enzyme_names <- unique(long_df$Enzyme)
+summary(long_df$`CLR Relative Abundance`)
 
-for (enzyme in enzyme_names) {
-  enzyme_data <- filter(long_df, Enzyme == enzyme)
-  plot_function(enzyme_data, enzyme)
-}
+my_plot <- ggplot(long_df, aes(x = Enzyme, y = `CLR Relative Abundance`, color = config)) +
+    geom_point(alpha = 0.8, size = 2, stroke = 0.3, position = position_jitterdodge(jitter.height = 0,
+                                                                                    jitter.width = 0.15)) +
+    stat_summary(aes(group = config),fun.min = function(x) {quantile(x, 0.25)}, fun.max = function(x) {quantile(x, 0.75)},
+                 size = 0.2, width = 0.3, geom = "errorbar", position = position_dodge(width = 0.8), color='black') +
+    stat_summary(aes(group = config), fun.min = median, fun.max = median,
+                 size = 0.2, width = 0.6, geom = "errorbar", position = position_dodge(width = 0.8), color='black') +
+    scale_color_manual(values = c("case" = "#cf5154", "control" = "#4e88aa")) + # Define custom colors
+    labs(x = "Enzyme", y = "CLR Relative Abundance") + # Label axes
+    theme_minimal() + # Minimal theme
+    coord_flip() + # Flip axes
+    
+    theme_linedraw() +
+    theme(panel.grid = element_blank(),
+          text = element_text(size = 14), # Base text size
+          axis.title = element_text(size = 16), # Title for axes
+          axis.text = element_text(size = 12), # Text for axis ticks
+          legend.title = element_text(size = 14), # Text for legend title
+          legend.text = element_text(size = 12),
+          legend.key.size = unit("0.35", "cm"),
+          legend.position = "bottom",
+          legend.margin = margin(-0.25, 0, 0, -0.2, unit = "cm"),
+          legend.justification = "left") +
+    guides(color = guide_legend(title = "Condition")) +# Add legend
+    scale_y_continuous(limits = c(-5, 5))
+
+ggsave("PRJEB10878_diff_abundance.tiff", plot = my_plot, width = 11, height = 8.5, units = "in", dpi = 300, type = "cairo")
 
