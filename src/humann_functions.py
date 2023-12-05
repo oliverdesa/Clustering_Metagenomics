31c3d9fe6a7dc371df25ea796bb4e7b889cfc28b
oliverdesa <oliver.desa@mail.utoronto.ca>
2023-12-05 13:11:16 -0500

    New parsing function made
    New Function created to parse XML files into useful metadata csv.

--- src/humann_functions.py
+++ src/humann_functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import xml.etree.ElementTree as ET
 
 def clean_table(tsv):
     """Clean the humann table for downstream analysis"""
@@ -34,8 +35,41 @@ def make_outputs(df, output_name):
 
     return output_name
 
+def xml_to_spreadsheet_sample(xml_file_path, output_file_path):
+    # Parse the XML file
+    tree = ET.parse(xml_file_path)
+    root = tree.getroot()
 
+    # Create a list to hold the data
+    data = []
 
+    # Iterate over each SAMPLE in the XML
+    for sample in root.findall('SAMPLE'):
+        # Initialize a dictionary for this sample
+        sample_info = {
+            'Alias': sample.get('alias'),
+            'Center Name': sample.get('center_name'),
+            'Accession': sample.get('accession'),
+            'BioSample ID': sample.find('.//PRIMARY_ID').text if sample.find('.//PRIMARY_ID') is not None else None,
+            'Taxon ID': sample.find('.//TAXON_ID').text if sample.find('.//TAXON_ID') is not None else None,
+            'Scientific Name': sample.find('.//SCIENTIFIC_NAME').text if sample.find('.//SCIENTIFIC_NAME') is not None else None
+        }
+
+        # Extract SAMPLE_ATTRIBUTES
+        for attr in sample.findall('.//SAMPLE_ATTRIBUTE'):
+            tag = attr.find('TAG').text if attr.find('TAG') is not None else None
+            value = attr.find('VALUE').text if attr.find('VALUE') is not None else None
+            if tag and value:
+                sample_info[tag] = value
+
+        # Append to data list
+        data.append(sample_info)
+
+    # Create a DataFrame from the data
+    df = pd.DataFrame(data)
+
+    # Export DataFrame to a CSV file
+    df.to_csv(output_file_path, index=False)
 
 
 
