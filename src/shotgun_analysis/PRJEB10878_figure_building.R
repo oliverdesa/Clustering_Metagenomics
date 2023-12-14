81c42408ca6b7dc3d1f6c506b17846f895dc758c
oliverdesa <107208238+oliverdesa@users.noreply.github.com>
2023-12-14 14:14:24 -0500

    added PCA code

--- src/shotgun_analysis/PRJEB10878_figure_building.R
+++ src/shotgun_analysis/PRJEB10878_figure_building.R
@@ -2,6 +2,9 @@ library(feather)
 library(ggplot2)
 library(tidyverse)
 library(Boruta)
+library(scales)
+library(vegan)
+library(pairwiseAdonis)
 
 czm_clr <- function(count_df) {
   
@@ -22,11 +25,38 @@ boruta <- function(count_df, meta_df) {
   
 }
 
-## Load the Data
-pooled_data <- read_feather('pooled_abundance_data.feather')
-pooled_data <- as.data.frame(pooled_data)  
+pooled_abun_df <- function(abun_df) {
+  
+  classes <- unlist(lapply(colnames(abun_df), function(col_name) {
+    strsplit(col_name, "_")[[1]][1]
+  }))
+  
+  pooled_abun_df <- aggregate(t(abun_df), by = list(classes), FUN = sum)
+  
+  rownames(pooled_abun_df) <- pooled_abun_df$Group.1
+  pooled_abun_df$Group.1 <- NULL
+  
+  pooled_abun_df <- data.frame(t(pooled_abun_df))
+  
+  colnames(pooled_abun_df) <- unlist(lapply(colnames(pooled_abun_df), function(col_name) {
+    sub(".", "-", col_name, fixed = TRUE)
+  }))
+  
+  pooled_abun_df
+  
+}
 
-metadata <- read_feather('metadf.feather')
+## Load the Data
+abundance_data <- read_feather('C:\\Users\\odesa\\Desktop\\CRCFinal\\PRJEB10878\\clean_joined_genefamilies_relab.feather')
+abundance_data <- abundance_data %>% column_to_rownames(var = "sample_id")
+
+pooled_data <- pooled_abun_df(abundance_data)
+
+# add sample_id back into the data frame
+pooled_data$SampleIdentifier <- rownames(pooled_data)
+
+
+metadata <- read_feather('C:\\Users\\odesa\\Desktop\\CRCFinal\\PRJEB10878\\PRJEB10878_metadata.feather')
 metadata <- as.data.frame(metadata)
 
 # add the 'config' column from metadata to the pooled_data df
@@ -86,28 +116,82 @@ my_plot <- ggplot(long_df, aes(x = Enzyme, y = `CLR Relative Abundance`, color =
     guides(color = guide_legend(title = "Condition")) +# Add legend
     scale_y_continuous(limits = c(-5, 5))
 
-## Apply and plot Boruta ##
+# ## Apply and plot Boruta ##
+# 
+# # Apply Boruta
+# boruta <- boruta(CLR_pooled_data, metadata)
+# 
+# # Plot Boruta
+# imp <- attStats(boruta)
+# 
+# # Create a dataframe for plotting
+# imp_df <- data.frame(
+#   Attribute = rownames(imp),
+#   Importance = imp$meanImp,
+#   Decision = imp$decision
+# )
+# 
+# print(imp_df)
+# 
+# 
+# # Plot using ggplot2
+# ggplot(imp_df, aes(x = reorder(Attribute, Importance), y = Importance, fill = Decision)) +
+#   geom_bar(stat = "identity") +
+#   coord_flip() + # Flip coordinates to make it a horizontal bar plot
+#   scale_fill_manual(values = c("Rejected" = "red", "Confirmed" = "green", "Tentative" = "yellow")) +
+#   theme_minimal() +
+#   labs(title = "Feature Importance from Boruta Analysis", x = "Features", y = "Importance (Z-Score)")
 
-# Apply Boruta
-boruta <- boruta(CLR_pooled_data, metadata)
 
-# Plot Boruta
-imp <- attStats(boruta)
+## PCA Analysis ##
 
-# Create a dataframe for plotting
-imp_df <- data.frame(
-  Attribute = rownames(imp),
-  Importance = imp$meanImp,
-  Decision = imp$decision
-)
+names(metadata)[names(metadata) == "Run"] <- "SampleIdentifier"
 
-print(imp_df)
+# add labels
+relab_pooled_labeled <- merge(pooled_data, metadata[, c("SampleIdentifier", "config")], by = "SampleIdentifier")
 
+# drop unwanted columns
+relab_pooled_labeled <- subset(relab_pooled_labeled, select = -c(SampleIdentifier, UNMAPPED, UC118))
 
-# Plot using ggplot2
-ggplot(imp_df, aes(x = reorder(Attribute, Importance), y = Importance, fill = Decision)) +
-  geom_bar(stat = "identity") +
-  coord_flip() + # Flip coordinates to make it a horizontal bar plot
-  scale_fill_manual(values = c("Rejected" = "red", "Confirmed" = "green", "Tentative" = "yellow")) +
+# Separate the labels from the data
+data <- relab_pooled_labeled[, -ncol(relab_pooled_labeled)]
+labels <- relab_pooled_labeled[, ncol(relab_pooled_labeled)]
+
+# Z-score normalization
+data_normalized <- as.data.frame(scale(data))
+
+# PCA
+pca_result <- prcomp(data_normalized, center = TRUE, scale. = TRUE)
+
+# Create a data frame for plotting
+pca_data <- as.data.frame(pca_result$x)
+pca_data$label <- labels
+
+# Plot
+pca_plot <- ggplot(pca_data, aes(x = PC1, y = PC2, color = label)) +
+  geom_point() +
+  stat_ellipse(type = "t", level = 0.95) +  # Adds ellipses for each group
   theme_minimal() +
-  labs(title = "Feature Importance from Boruta Analysis", x = "Features", y = "Importance (Z-Score)")
+  annotate("text", x = Inf, y = Inf, label = annotation_text, hjust = 1, vjust = 1, size = 3.5) +
+  theme(plot.margin = margin(1, 1, 1, 1, "cm"))
+
+pca_plot <- pca_plot + 
+  theme(panel.background = element_rect(fill = "white"),
+        plot.background = element_rect(fill = "white", colour = "white"),
+        legend.background = element_rect(fill = "white"),
+        plot.title = element_text(size = 14, hjust = 0.5, color = "black"),
+        legend.text = element_text(color = "black"),
+        legend.title = element_text(color = "black"))
+
+ggsave("PCA_plot_10878.png", plot = pca_plot, width = 10, height = 10, units = "in")
+
+
+# Permanova
+distance_matrix <- vegdist(data_normalized, method = "euclidean")
+permanova_result <- adonis2(distance_matrix ~ labels)
+print(permanova_result)
+
+pairwise_results <- pairwise.adonis(distance_matrix, labels)
+print(pairwise_results)
+
+annotation_text <- "Case vs Control R2 = 0.00886, p value = 0.322"
