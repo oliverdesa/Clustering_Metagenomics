10a304f1c42b02c9aa222e1056dc6556974c3457
oliverdesa <oliver.desa@mail.utoronto.ca>
2024-01-11 10:26:02 -0500

    Figure Generation for pooled DRA008156
    Added a function to generate a pooled DA figure for 8156. Figure saved to figures dir.

--- src/shotgun_analysis/DRA008156_analysis.R
+++ src/shotgun_analysis/DRA008156_analysis.R
@@ -100,5 +100,50 @@ results <- Maaslin2(
   reference=c("Group,Healthy")
 )
 
+# make indices of metadata_7774 into column
+metadata_DRA008156$run_accession <- rownames(metadata_DRA008156)
+filt_pooled_abun_df_DRA008156$run_accession <- rownames(filt_pooled_abun_df_DRA008156)
+
+# Add sample_title to pooled_abun_df_7774 where run_accession matches
+filt_pooled_abun_df_DRA008156$sample_title <- metadata_DRA008156$Group[match(filt_pooled_abun_df_DRA008156$run_accession, 
+                                                                             metadata_DRA008156$run_accession)]
+
+# convert to long df format
+long_clr_pooled<- filt_pooled_abun_df_DRA008156 %>% 
+  pivot_longer(
+    cols = c(Amidase:Muramidase), names_to = "Enzyme", 
+    values_to = "Relative Abundance")
+
+# Plot the differential abundances and save to a tiff for pub
+
+my_plot <- ggplot(long_clr_pooled, aes(x = Enzyme, y = `Relative Abundance`, color = sample_title)) +
+  geom_point(alpha = 0.8, size = 2, stroke = 0.3, position = position_jitterdodge(jitter.height = 0,
+                                                                                  jitter.width = 0.15)) +
+  stat_summary(aes(group = sample_title), fun.min = function(x) {quantile(x, 0.25)}, fun.max = function(x) {quantile(x, 0.75)},
+               size = 0.2, width = 0.3, geom = "errorbar", position = position_dodge(width = 0.8), color='black') +
+  stat_summary(aes(group = sample_title), fun.min = median, fun.max = median,
+               size = 0.2, width = 0.6, geom = "errorbar", position = position_dodge(width = 0.8), color='black') +
+  scale_color_manual(values = c("Cancer" = "#cf5154", "Healthy" = "#4e88aa")) +
+  labs(x = "Enzyme", y = "Relative Abundance") +
+  theme_minimal() +
+  coord_flip() +
+  theme_linedraw() +
+  theme(panel.grid = element_blank(),
+        text = element_text(size = 14),
+        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 12),
+        legend.title = element_text(size = 14),
+        legend.text = element_text(size = 12),
+        legend.key.size = unit("0.35", "cm"),
+        legend.position = "bottom",
+        legend.margin = margin(-0.25, 0, 0, -0.2, unit = "cm"),
+        legend.justification = "left") +
+  guides(color = guide_legend(title = "Condition"))+
+
+  ggsave("C:\\Users\\odesa\\Desktop\\Code\\CRC-Final\\Figures\\DRRA008156_DA_pooled.tiff",
+         width = 10, height = 10, units = "in", device = 'tiff', dpi = 295)
+
+#print(my_plot)
 
 
+  
