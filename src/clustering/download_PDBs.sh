b3fe1d0c32e6ef6b79812eb6ec54788c3dfeb13a
oliverdesa <oliver.desa@mail.utoronto.ca>
2024-01-16 16:24:45 -0500

    Update download_PDBs.sh
    Changed the script to be able to download specified enzymes PDBs from multiple dirs

--- src/clustering/download_PDBs.sh
+++ src/clustering/download_PDBs.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-while IFS= read -r line
-do
-    # Construct the download URL
-    url="https://alphafold.ebi.ac.uk/files/AF-${line}-F1-model_v4.pdb"
+# Directory containing the input files
+input_dir="/media/oliver/PGH_Backup/clustering"
 
-    # Download the file
-    curl -o "AF-${line}-F1-model_v4.pdb" "$url"
-done < "uniprot_ids.txt"
+# Loop over each file in the input directory
+for file in "$input_dir"/*; do
+    # Extract the base name of the file
+    base_name=$(basename "$file" .txt)
+
+    # Create a directory with the base name
+    mkdir -p "/media/oliver/PGH_Backup/clustering/$base_name"
+
+    # Loop over lines in the file
+    while IFS= read -r line; do
+        # Construct the download URL
+        url="https://alphafold.ebi.ac.uk/files/AF-${line}-F1-model_v4.pdb"
+
+        # Download the file and save it in the directory named after the base name
+        curl -o "${base_name}/AF-${line}-F1-model_v4.pdb" "$url"
+    done < "$file"
+done
+
 
