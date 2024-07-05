a1c4091109aa21de8e81d1be8134b4342508a5ea
oliverdesa <oliver.desa@mail.utoronto.ca>
2024-07-05 12:08:01 -0400

    re-clustering for all PGHs in IBD data
    - Also ran DA testing for IBD data with all PGHs included

--- src/shotgun_analysis/IBD_DA_testing.R
+++ src/shotgun_analysis/IBD_DA_testing.R
@@ -36,21 +36,21 @@ filtered_metadata_ibd <- metadata_ibd %>%
 filtered_metadata_ibd <- filtered_metadata_ibd %>% column_to_rownames(var = "External.ID")
 
 # Patterns to search for
-patterns <- c("^DL.endopeptidase")
+# patterns <- c("^DL.endopeptidase")
 
 # Combine patterns into a single regular expression
-pattern <- paste(patterns, collapse = "|")
+# pattern <- paste(patterns, collapse = "|")
 
-selected_columns <- grep(pattern, names(abundance_data_ibd_unique), value = TRUE)
+# selected_columns <- grep(pattern, names(abundance_data_ibd_unique), value = TRUE)
 
-abundance_data_ibd_unique <- abundance_data_ibd_unique[, selected_columns]
+# abundance_data_ibd_unique <- abundance_data_ibd_unique[, selected_columns]
 
 ## Run Maaslin2 ##
 
 results <- Maaslin2(
   input_data = abundance_data_ibd_unique,
   input_metadata = filtered_metadata_ibd,
-  output = "/Volumes/PGH-Backup/ibd_data/maaslin2/maaslin2_results_ibd",
+  output = "/Volumes/PGH-Backup/ibd_data/maaslin2/maaslin2_results_ibd/all_PGHs",
   fixed_effects = c("diagnosis"),
   random_effects = NULL,
   normalization = "none", 
